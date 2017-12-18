3af44693bcd35fa954d377dcb45c406ed5155ae2
Won <wonpark89@gmail.com>
2017-12-18 01:37:25 -0800

    connection with databse_url

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 // Update with your config settings.
 const sharedConfig = {
   client: 'postgresql',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
   migrations: {
     tableName: 'knex_migrations',
     directory: './db/migrations'
@@ -25,9 +24,8 @@ module.exports = {
 
   production: {
     ...sharedConfig,
-    connection: {
-      database: 'order_pro'
-    }
+    connection: 'postgres://gzoddbmqvdtawa:b0943e976a7083f11ba591fc3a9e904ff75b6e064d41d25cecedd28b64187ae6@ec2-107-21-201-57.compute-1.amaz
+onaws.com:5432/d5cnl9ai2tsfkj'
   }
 
 };
